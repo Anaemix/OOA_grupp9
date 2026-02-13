a56a3f1b6f4d67b89b3b8d22351ef698e19321b7
Najib <najibal@chalmers.se>
2026-02-13 14:59:39 +0100

    added getMessages

--- server/DatabaseHandler.java
+++ server/DatabaseHandler.java
@@ -1,11 +1,8 @@
 package server;
 
-
 import java.sql.*;
 import java.util.*;
-
 import java.time.Instant;
-
 import client.User;
 import client.Message;
 import client.Chat;
@@ -31,7 +28,6 @@ public class DatabaseHandler {
         }
     }
 
-
     private void enableForeignkeys() {
 
         String statement = "PRAGMA foreign_keys = ON;";
@@ -47,7 +43,6 @@ public class DatabaseHandler {
     private void createTablesIfNotExist() {
         
         
-        
         // SQL statements to create the tables
         String createUsersTable = "CREATE TABLE IF NOT EXISTS users (" +
                                   "name TEXT PRIMARY KEY)";
@@ -60,13 +55,14 @@ public class DatabaseHandler {
                                      "sender TEXT NOT NULL, " +
                                      "chatname TEXT NOT NULL," + 
                                      "content TEXT, " +
-                                     "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                                     "id INTEGER PRIMARY KEY AUTOINCREMENT," +
                                      "FOREIGN KEY (sender) REFERENCES users(name), " +
                                      "FOREIGN KEY (chatname) REFERENCES chats(name))";
 
         String createChatUsersTable = "CREATE TABLE IF NOT EXISTS chat_users (" +
-                                      "chatname TEXT NOT NULL, " +
-                                      "username TEXT NOT NULL, " +
+                                      "chatname TEXT  NOT NULL, " +
+                                      "username TEXT  NOT NULL, " +
+                                      "PRIMARY KEY (username , chatname)," +
                                       "FOREIGN KEY (chatname) REFERENCES chats(name), " +
                                       "FOREIGN KEY (username) REFERENCES users(name))";
         
@@ -83,8 +79,6 @@ public class DatabaseHandler {
         }
     }
 
-    
-
     // Close the database connection
     public void closeConnection() {
         try {
@@ -97,6 +91,7 @@ public class DatabaseHandler {
         }
     }
 
+    // This method removes a user from the chatusers table 
     public void removeUserFromChat(User user, String chatName) {
         String remove = "DELETE FROM chat_users WHERE username = ? AND chatname = (?)";
         try(PreparedStatement pstmt = connection.prepareStatement(remove)) {
@@ -111,24 +106,48 @@ public class DatabaseHandler {
         }
     }
 
+    //This method adds a user to the users table
     public void addUser(User user) {
-
-    
+        
         String adduser = "INSERT OR IGNORE INTO users (name) VALUES (?)";
         try(PreparedStatement pstmt = connection.prepareStatement(adduser)) {
             pstmt.setString(1, user.getName());
             pstmt.executeUpdate();
-
+            
         }catch(SQLException e) {
             e.printStackTrace();
         }
     }
 
+    //This method adds a chat to the chats table
+    public void addChat(String chatname) {
+        String addchat = "INSERT OR IGNORE INTO chats (name) VALUES (?)";
+        try (PreparedStatement pstmt = connection.prepareStatement(addchat)) {
+            pstmt.setString(1, chatname);
+            pstmt.executeUpdate();
+            System.out.println("chat added to chats table");
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    //This method adds a user to chat if they are not part of the chat already
+    public void addUserToChat(User user, String chatname) {
+        String adduser = "INSERT OR IGNORE INTO chat_users (username,chatname) VALUES (?,?)"; 
 
-    public void addMessage( String chatName, Message message) {
+        try(PreparedStatement stmt = connection.prepareStatement(adduser)){
+            stmt.setString(1, user.getName());
+            stmt.setString(2, chatname);
+            stmt.executeUpdate();
 
-        
-        
+        }
+            
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    //This method adds a message to the messages table 
+    public void addMessage(String chatName, Message message) {        
         
         try(PreparedStatement insertmessage = connection.prepareStatement("INSERT INTO messages (timestamp, content, sender, chatname) VALUES (?,?,?,?)")) {
             
@@ -146,49 +165,29 @@ public class DatabaseHandler {
         }
     } 
     
+    //This method returns an array of all messages sent to a chat after a specific timestamp
+    public ArrayList<Message> getMessages(String chatname, Instant time){
 
-    
-    private boolean checkIfUserExists(User user) {
+        ArrayList<Message> messagelist = new ArrayList<Message>();
+        try(PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM messages WHERE chatname = ? AND timestamp > (?) ORDER BY timestamp ASC")) {
+            pstmt.setString(1,chatname);
+            pstmt.setLong(2, time.getEpochSecond());
+            ResultSet rs = pstmt.executeQuery();
 
-        String check = "SELECT EXISTS( SELECT 1 FROM users where name = (?)";
-
-        try(PreparedStatement stmt = connection.prepareStatement(check);) {
-            stmt.setString(1,user.getName());
-            
-
-            ResultSet rs = stmt.executeQuery();
-
-            if (rs.next()) {
-                return true;
+            while (rs.next()) {
+                long timestamp = rs.getLong("timestamp");
+                String message = rs.getString("content");
+                String sender = rs.getString("sender");
+                messagelist.add((new Message(message, Instant.ofEpochSecond(timestamp) ,new User(0, sender))));
             }
-            
+
         }
         catch(SQLException e) {
             e.printStackTrace();
         }
-        return false;
+        return messagelist;
     }
 
-    private boolean checkIfChatExists(String chat) {
-
-        String check = "SELECT EXISTS( SELECT 1 FROM chats where name = (?)";
-
-        try(PreparedStatement stmt = connection.prepareStatement(check);) {
-            stmt.setString(1,chat);
-            
-
-            ResultSet rs = stmt.executeQuery();
-
-            if (rs.next()) {
-                return true;
-            }
-            
-        }
-        catch(SQLException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
 
     // a private method used to add messages to a chat object 
     private void getMessagesInChat(Chat chat, String chatname) {
@@ -205,80 +204,35 @@ public class DatabaseHandler {
                 chat.addMessage(new Message(message, Instant.ofEpochSecond(timestamp) ,new User(0, sender)));
                 
             }
-
         }
-
 
         catch(SQLException e){
             e.printStackTrace();
         }
 
     }
+    //This method returns a chat object
     public Chat getChat(String chatname){
     
         Chat chat = new Chat(chatname);
         String getusers = "SELECT username FROM chat_users WHERE chatname = (?)";
-
-
         try(PreparedStatement pstmt = connection.prepareStatement(getusers)) {
             pstmt.setString(1, chatname);
             ResultSet rs = pstmt.executeQuery();
 
             while(rs.next()) {
                 String username = rs.getString("username");
-                chat.addUser(new User(0, username));
-                
+                chat.addUser(new User(0, username)); 
             }
 
         }
-
-
         catch(SQLException e){
             e.printStackTrace();
         }
         getMessagesInChat(chat, chatname);
         return chat;
-        
     }
-
-
-    public void addUserToChat(User user, String chatname) {
-        String adduser = "INSERT OR IGNORE INTO chat_users (username,chatname) VALUES (?,?)"; 
-
-        try(PreparedStatement stmt = connection.prepareStatement(adduser)){
-            stmt.setString(1, user.getName());
-            stmt.setString(2, chatname);
-            stmt.executeUpdate();
-
-        }
-            
-        catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void addChat(String chatname) {
-        String addchat = "INSERT OR IGNORE INTO chats (name) VALUES (?)";
-        try (PreparedStatement pstmt = connection.prepareStatement(addchat)) {
-            pstmt.setString(1, chatname);
-            pstmt.executeUpdate();
-            System.out.println("chat added to chats table");
-        }
-        catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-    public void Connect(User user,String chat){
-        if (checkIfChatExists(chat)) {
-            addUserToChat(user, chat);
-        }
-        else {
-            addChat(chat);
-            addUserToChat(user, chat);
-        }
-
-
-    }
+    //This method returns an arraylist of allchats that a user is a part of
     public  ArrayList<String> getAllChats(User user){
         ArrayList<String> list = new ArrayList<>();
         String getchatname = "SELECT chatname FROM chat_users where username = (?)";
@@ -299,39 +253,16 @@ public class DatabaseHandler {
         return list;
     }
 
-
     public static void main(String[] args) {
         
-    
+
     }
 
     
 }
 
 
+// a user should be able to create a username or login
+// a user should be able to join or create a chat
+// no duplicate users or duplicate chats
 
-     
-
-// All of these methods should check if the user exists else they should add them, they should do the same thing for creating/joining a chat if the
-//chat is not they are trying to join does not exist then create it 
-
-// I should add select chat from a chatlist, select user from a userlist 
-
-// addMessage(userName, chatName) done 
-// add/joinchat(chatName,userName) done 
-// check if chat exists 
-// disconnect(userName, chatName) done
-//getChats(userName) 
-//getChat(chatname) done
-   
-
-
-
-//adduser -> addUser
-//sendMessage -> addMessage
-//addchat -> addChat
-// enableforeignkeys = true
-// dropalltables removed
-
-
-//add timestamp to messages 
