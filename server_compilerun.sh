3ec435cb99b1288e95a02a91b503c5f2e5bbd25c
cabbage <henning.dahlen@gmail.com>
2026-02-18 22:06:20 +0100

    updated compile script for server

--- server_compilerun.sh
+++ server_compilerun.sh
@@ -1,2 +1,13 @@
-javac -cp .:libs/gson-2.13.2.jar:libs/sqlite-jdbc-3.51.1.0.jar server/*.java client/*.java -Xdiags:verbose
-java -cp .:libs/sqlite-jdbc-3.51.1.0.jar:libs/gson-2.13.2.jar server.Server
+javac -cp .:\
+libs/gson-2.13.2.jar:\
+libs/sqlite-jdbc-3.51.1.0.jar:\
+libs/Java-WebSocket-1.6.0.jar:\
+libs/slf4j-api-2.0.9.jar\
+ server/*.java client/*.java -Xdiags:verbose
+java -cp .:\
+libs/gson-2.13.2.jar:\
+libs/sqlite-jdbc-3.51.1.0.jar:\
+libs/Java-WebSocket-1.6.0.jar:\
+libs/slf4j-api-2.0.9.jar\
+ server.Server
+
