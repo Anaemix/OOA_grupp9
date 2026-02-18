17592e0a5d47fe1ff7db8591b8c8b97cefa0a110
maincabbage <henning.dahlen@gmail.com>
2026-02-18 21:36:55 +0100

    simplified connect&disconnect handlers and added documentation

--- server/ConnectHandler.java
+++ server/ConnectHandler.java
@@ -6,7 +6,6 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 
 import com.google.gson.Gson;
@@ -14,47 +13,54 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 
 import java.time.Instant;
-import java.util.Set;
 
+/**
+ * This class the connect http handler which handles the joining/connecting to a chat room.
+ * 
+ * @author Henning
+ * @version 0.1
+ */
 public class ConnectHandler implements HttpHandler {
+    /** The handler used for database persistence. */
     private final DatabaseHandler db;
+    /** Gson object used for deserialization of json. */
     private static Gson gson = new GsonBuilder().registerTypeAdapter(Instant.class, new Gson_InstantTypeAdapter()).create();
-    
+
+    /**
+     * Constructor 
+     * @param databaseHandler handles the database connection, writing/reading.
+     */
     public ConnectHandler(DatabaseHandler databaseHandler) {
         this.db = databaseHandler;
     }
 
+    /**
+     * This handles the http request depending on the Request type. 
+     * Will respond with statuscodes will never return any data to the requestee. <br>
+     * -200 OK <br>
+     * -400 Bad Request, if an exception was raised in the json parsing or addition of the user in the database <br>
+     * -405 Method Not Allowed, if POST request method was not used <br>
+     * @param httpexchange http exchange to be handled by the function
+     */
     public void handle(HttpExchange httpexchange) throws IOException {
         if (httpexchange.getRequestMethod().equalsIgnoreCase("POST")) {
-            String response = "0";
-
             try {
                 InputStream is = httpexchange.getRequestBody();
                 String body = new String(is.readAllBytes(), StandardCharsets.UTF_8);
 
                 JsonObject jsonobject = gson.fromJson(body, JsonObject.class);
-                Set<String> keys = jsonobject.keySet();
 
+                User user = gson.fromJson(jsonobject.getAsJsonObject("user"), User.class);
+                String chat = jsonobject.get("chat").getAsString();
+                
+                db.addUser(user);
+                db.addChat(chat);
+                db.addUserToChat(user, chat);
 
-                if (keys.contains("user") && keys.contains("chat")) {
-                    User user = gson.fromJson(jsonobject.getAsJsonObject("user"), User.class);
-                    String chat = jsonobject.get("chat").getAsString();
-                    
-                    db.addUser(user);
-                    db.addChat(chat);
-                    db.addUserToChat(user, chat);
-
-                    response = "true";
-                }
+                httpexchange.sendResponseHeaders(200, -1);
             } catch (Exception e) {
                 e.printStackTrace();
-            }
-
-            httpexchange.getResponseHeaders().add("Content-Type", "application/json");
-            httpexchange.sendResponseHeaders(200, response.getBytes().length);
-            
-            try (OutputStream os = httpexchange.getResponseBody()) {
-                os.write(response.getBytes());
+                httpexchange.sendResponseHeaders(400, -1);
             }
         } else {
             httpexchange.sendResponseHeaders(405, -1);
