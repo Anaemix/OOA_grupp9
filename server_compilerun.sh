0aeaa2f24a29d109d050acb2122e04fca478565f
cabbage <henning.dahlen@gmail.com>
2026-02-24 15:41:30 +0100

    Added image support

--- server_compilerun.sh
+++ server_compilerun.sh
@@ -1,13 +1,7 @@
-javac -cp .:\
+javac -d bin2 -cp .:\
 libs/gson-2.13.2.jar:\
 libs/sqlite-jdbc-3.51.1.0.jar:\
 libs/Java-WebSocket-1.6.0.jar:\
 libs/slf4j-api-2.0.9.jar\
  server/*.java client/*.java -Xdiags:verbose
-java -cp .:\
-libs/gson-2.13.2.jar:\
-libs/sqlite-jdbc-3.51.1.0.jar:\
-libs/Java-WebSocket-1.6.0.jar:\
-libs/slf4j-api-2.0.9.jar\
- server.Server
-
+ (cd bin2 && java -cp .:libs/gson-2.13.2.jar:libs/sqlite-jdbc-3.51.1.0.jar:libs/Java-WebSocket-1.6.0.jar:libs/slf4j-api-2.0.9.jar server.Server)
