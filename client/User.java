78c07a561e692695c537ffcfe1c167d7ffb415a8
Hughul <132865466+Hughul@users.noreply.github.com>
2026-02-27 11:12:27 +0100

    added javadoc comments to User

--- client/User.java
+++ client/User.java
@@ -1,5 +1,7 @@
 package client;
 
+import java.util.Objects;
+
 /**
  * User - Represents a chat user.
  * Stores user information for identifying message senders.
@@ -7,37 +9,58 @@ package client;
 public class User {
     private String name;
 
+    /**
+     * Constructs a new User with the specified name.
+     * @param name The display name of the user.
+     */
     public User(String name) {
         this.name = name;
     }
 
     /**
      * Returns the display name of the user.
+     * @return The user's name as a String.
      */
     public String getName() {
         return name;
     }
 
     /**
-     * Sets the display name of the user.
+     * Sets or updates the display name of the user.
+     * @param name The new name to assign to this user.
      */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Returns a string representation of the user, which is their name.
+     * @return The users name.
+     */
     @Override
     public String toString() {
         return name;
     }
 
+    /**
+     * Compares this user to another object for equality.
+     * Two users are considered equal if they share the same name.
+     * @param obj The object to compare with this User.
+     * @return true if the names are identical; false otherwise.
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
         if (obj == null || getClass() != obj.getClass()) return false;
         User user = (User) obj;
-        return name == user.name;
+        return Objects.equals(name, user.name);
     }
 
+    /**
+     * Generates a hash code for this user based on their name.
+     * This ensures that Users can be stored and retrieved correctly.
+     * @return The hash code of the user's name.
+     */
     @Override
     public int hashCode() {
         return name.hashCode();
